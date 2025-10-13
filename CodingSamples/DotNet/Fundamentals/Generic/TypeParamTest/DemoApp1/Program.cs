b6217e312bac00fd6fbb7a58c6324e82d419e18e
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2025-10-13 15:32:34 +0530

    Add files via upload

---
+++ CodingSamples/DotNet/Fundamentals/Generic/TypeParamTest/DemoApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Serialization;
+
+class Program
+{
+    //generic method with type-parameter T
+    static T Select<T>(int index, T first, T second)
+    {
+        if((index % 2) == 1)
+            return first;
+        return second;
+    }
+
+ 
+
+    static void Main(string[] args)
+    {
+        if(args.Length > 0 && int.TryParse(args[0], out int s))
+        {
+            string ss = Select(s, "June", "July");
+            Console.WriteLine($"Selected string = {ss}");
+            double sd = Select(s, 7.25, 9.75);
+            Console.WriteLine($"Selected double = {sd}");
+            //Select(s, ss, sd);
+        }
+        else
+        {
+
+        }
+    }
+}
