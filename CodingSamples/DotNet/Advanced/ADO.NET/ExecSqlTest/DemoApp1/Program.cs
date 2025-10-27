0f4e9daa007330a1485570c04a8799ffe4ee147a
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2025-10-27 18:08:19 +0530

    Add files via upload

---
+++ CodingSamples/DotNet/Advanced/ADO.NET/ExecSqlTest/DemoApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.Sqlite;
+
+using var connection = new SqliteConnection("Data Source=shop.db");
+connection.Open();
+using var command = connection.CreateCommand();
+if(args.Length == 0)
+{
+    command.CommandText = "SELECT UserName, Address, Credit FROM CustomerInfo";
+    using var reader = command.ExecuteReader();
+    while(reader.Read())
+        Console.WriteLine("{0}\t{1}\t{2}", reader.GetString(0), reader.GetString(1), reader.GetDecimal(2));
+}
+else if(args[0].Length == 5)
+{
+    command.CommandText = $"UPDATE CustomerInfo SET Credit=Credit+100 WHERE UserName='{args[0]}'";
+    int n = command.ExecuteNonQuery();
+    if(n == 0)
+        Console.WriteLine("No such customer!");
+}
