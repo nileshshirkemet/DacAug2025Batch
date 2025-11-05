cfd655aff84f45845f8d6f13c3a42406cbc67800
nileshshirkemet <43514175+nileshshirkemet@users.noreply.github.com>
2025-11-05 13:03:09 +0530

    Add files via upload

---
+++ CodingSamples/Java/Fundamentals/Inheritance/SubclassTest/demo2/Interval.java
@@ -0,0 +1,42 @@
+class Interval {
+
+    private int min;
+    private int sec;
+
+    public Interval(int m, int s) {
+        min = m + s / 60;
+        sec = s % 60;
+    }
+
+    public int minutes() {
+        return min;
+    }
+
+    public int seconds() {
+        return sec;
+    }
+
+    public int time() {
+        return 60 * min + sec;
+    }
+
+    //overriding toString() method of java.lang.Object (default superclass)
+    //to return string representation of this object
+    public String toString() {
+        if(sec < 10)
+            return min + ":0" + sec;
+        return min + ":" + sec;
+    }
+
+    public int hashCode() {
+        return min + sec;
+    }
+
+    public boolean equals(Object other) {
+        if(other instanceof Interval){
+            Interval that = (Interval)other;
+            return this.min == that.min && this.sec == that.sec;
+        }
+        return false;
+    }
+}
